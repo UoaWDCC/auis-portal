310f07eccff4932e1a22fef59795c5f6e8606f25
Karmveer Singh <karmveer.nz@gmail.com>
2024-04-06 23:30:07 +1300

    Adding routes and router provider in main.tsx which sends <Outlet /> to app.tsx which has Header and Footer on top and bottom prespectively. Also changed pages name to screens and added components directory

---
+++ web/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+    const currentTime = new Date().getFullYear();
+    return (
+        <footer>
+            <h1>Footer | {currentTime}</h1>
+        </footer>
+    )
+}
+
+export default Footer
