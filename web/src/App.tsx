a6842de310b10ec1f94673857bd4aa30ae0ea893
emm4chen <117613372+emm4chen@users.noreply.github.com>
2024-05-17 16:51:18 +1200

    Created Exec Screen

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Exec from './components/ExecInfo';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
         <main className="py-3">
           <div className="max-w-6xl mx-auto px-4">
             <Outlet />
+            <Exec/>
           </div>
         </main>
         <Footer />
