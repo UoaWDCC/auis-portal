6c5bfd6f78550131ae39a33e654c9adda9c026fc
Karmveer Singh <karmveer.nz@gmail.com>
2024-06-04 00:30:36 +1200

    Connected local postgress instance and a post method that adds user with email

--- api/controller/authController.ts
+++ api/controller/authController.ts
@@ -1,10 +1,47 @@
 import { Request, Response } from 'express';
 import asyncHandler from "../middleware/asyncHandler";
 
+import db from '../db/db';
+import { users } from '../models/users';
+import { sql } from 'drizzle-orm';
+
 export const signUp = asyncHandler(async (req: Request, res: Response) => {
     throw new Error("Not implemented yet");
 });
 
 export const logIn = asyncHandler(async (req: Request, res: Response) => {
     throw new Error("Not implemented yet");
+});
+
+export const clerkSignUp = asyncHandler(async (req: Request, res: Response) => {
+    const { email } = req.body;
+    
+
+    const newUser = await db.insert(users).values({
+      email,
+      uoa_id: '',  // default value
+      upi: '',     // default value
+      institution: 'Unknown',  // default value
+      year: 'Unknown',         // default value
+      study_field: '',         // default value
+      name: 'Unknown',         // default value
+      is_admin: false,
+      is_paid: false,
+      is_info_confirmed: false,
+    }).returning({
+      user_id: users.user_id,
+      email: users.email,
+      uoa_id: users.uoa_id,
+      upi: users.upi,
+      institution: users.institution,
+      year: users.year,
+      study_field: users.study_field,
+      name: users.name,
+      is_admin: users.is_admin,
+      is_paid: users.is_paid,
+      is_info_confirmed: users.is_info_confirmed,
+      created_at: users.created_at,
+    });
+
+    res.json(newUser[0]);
 });
