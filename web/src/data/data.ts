e2e069cac5230657a303acaa8f0903845fec16fe
Naren Rohan <nroh555@aucklanduni.ac.nz>
2024-06-06 23:57:35 +1200

    refactored code and fixed bug

---
+++ web/src/data/data.ts
@@ -0,0 +1,11 @@
+import { Exec } from "../types/types";
+
+export const execs: Exec[] = [
+  {
+    id: 1,
+    image: "Gury",
+    position: "Gury is the best",
+    name: "Guryash Da Goat",
+    bio: "This guy is too good to be true",
+  },
+];
