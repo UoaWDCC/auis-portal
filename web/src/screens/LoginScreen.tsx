0dd1f0b068ccb437182fc3443b6c0a2f45650bcb
Karmveer Singh <karmveer.nz@gmail.com>
2024-06-11 18:49:39 +1200

    Fixed gradiant for auth pages

--- web/src/screens/LoginScreen.tsx
+++ web/src/screens/LoginScreen.tsx
@@ -2,12 +2,16 @@ import { SignIn } from "@clerk/clerk-react";
 
 function LoginScreen() {
     return (
-        <div className="flex-grow flex flex-col h-full ">
-            <div className="flex flex-grow justify-center ">
-                <div className="flex flex-1 items-center justify-center ">
+        <div className="flex-grow flex flex-col h-full">
+            <div className="flex flex-grow justify-center">
+                <div className="flex flex-1 items-center justify-center">
                     <SignIn />
                 </div>
-                <div className="flex flex-col flex-1 items-center justify-center text-center p-4 bg-gradient-to-b from-blue-300 to-white">
+                <div className="flex flex-col flex-1 items-center justify-center text-center p-4"
+                     style={{
+                         background: 'linear-gradient(to top, #ccf2ff 50%, #ffffff 100%)',
+                     }}
+                >
                     <div className="mb-5">
                         <img src="/src/assets/react.svg" alt="Descriptive Text" className="min-w-1000 min-h-1000" />
                     </div>
--- web/src/screens/SignUpScreen.tsx
+++ web/src/screens/SignUpScreen.tsx
@@ -5,7 +5,11 @@ function SignUpScreen() {
         <div className="flex-grow flex flex-col h-full ">
             <div className="flex flex-grow justify-center ">
 
-                <div className="flex flex-col flex-1 items-center justify-center text-center p-4 bg-gradient-to-br from-primary-orange to-white">
+                <div className="flex flex-col flex-1 items-center justify-center text-center p-4 "
+                     style={{
+                         background: 'linear-gradient(to top, #fda642 50%, #ffffff 100%)',
+                     }}
+                >
                     <div className="mb-5">
                         <img src="/src/assets/react.svg" alt="Descriptive Text" className="min-w-1000 min-h-1000" />
                     </div>
