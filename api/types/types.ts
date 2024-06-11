2382aa949ddb5506fb7776f27ce75da7354cfdc3
Naren Rohan <nroh555@aucklanduni.ac.nz>
2024-06-12 02:45:00 +1200

    Added automatic type generator for FE from BE

--- api/types/types.ts
+++ api/types/types.ts
@@ -3,3 +3,18 @@ export interface User {
   email: string;
   year_of_study: number;
 }
+
+export interface UserComplex {
+  user_id: number;
+  email: string;
+  uoa_id?: string;
+  upi?: string;
+  institution?: string;
+  year?: string;
+  study_field?: string;
+  name: string;
+  is_admin: boolean;
+  is_paid: boolean;
+  is_info_confirmed: boolean;
+  created_at: Date;
+}
