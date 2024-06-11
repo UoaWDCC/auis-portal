bd1dd7e6da417d563d18061218405d415719b7b5
Naren Rohan <nroh555@aucklanduni.ac.nz>
2024-06-12 00:41:13 +1200

    Created demo backend route for user

---
+++ api/routes/userRoutes.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { getAllUsers } from "../controller/userController";
+
+const router = Router();
+
+router.get("/users", getAllUsers);
+export default router;
---
+++ api/types/types.ts
@@ -0,0 +1,5 @@
+export interface User {
+  name: string;
+  email: string;
+  year_of_study: number;
+}
