1a0281e7f3d8ff89016b99196dc731c29105c72d
Naren Rohan <nroh555@aucklanduni.ac.nz>
2024-06-20 01:36:06 +1200

    Generated schemas

---
+++ api/drizzle-schema.config.ts
@@ -0,0 +1,14 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./schemas/*",
+  out: "./schemas",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+  verbose: true,
+  strict: true,
+});
