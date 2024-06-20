029e3f17aae3c6d409b32db46cfd57ba37526a98
Guryash <gmat224@aucklanduni.ac.nz>
2024-06-20 15:33:50 +1200

    fix: footer

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -5,8 +5,8 @@ export default function Footer() {
 	const currentYear = new Date().getFullYear();
 	return (
 		<>
-			<footer className="w-full bg-white px-10 absolute bottom-0">
-				<div className="flex flex-col lg:flex-row justify-between items-center">
+			<footer className="w-full bg-white px-10">
+				<div className="flex flex-col lg:flex-row justify-between items-center ">
 					<div className="flex items-center space-x-4">
 						<img src={peacockLogo} alt="Peacock Logo" />
 						<span className="text-black font-bold">&copy; AUIS {currentYear}. All Rights Reserved</span>
