e75b61021189f2fbdbf0039ceb2ea2d846de0731
Guryash <gmat224@aucklanduni.ac.nz>
2024-06-29 13:36:56 +1200

    feat: updated sign-up styling

--- web/src/screens/SignUpScreen.tsx
+++ web/src/screens/SignUpScreen.tsx
@@ -1,28 +1,21 @@
 import { SignUp } from "@clerk/clerk-react";
+import Header from "@components/Header";
+import auisLogo from "../assets/peacock_white_inner_hd.png";
 
 function SignUpScreen() {
-    return (
-        <div className="flex-grow flex flex-col h-full ">
-            <div className="flex flex-grow justify-center ">
-
-                <div className="flex flex-col flex-1 items-center justify-top text-center pt-20 "
-                     style={{
-                         background: 'linear-gradient(to top, #fda642 50%, #ffffff 100%)',
-                     }}
-                >
-                    <div className="mb-5">
-                        <img src="/src/assets/peacock.png" alt="Descriptive Text" className="max-w-[300px] max-h-[300px] " />
-                    </div>
-                    <div>
-                        <img src="/src/assets/AUIS_black_3.png" alt="Descriptive Text" className="max-w-xs max-h-xs" />
-                    </div>
-                </div>
-                <div className="flex flex-1 items-center justify-center ">
-                    <SignUp afterSignUpUrl="/user-info"/>
-                </div>
-            </div>
+  return (
+    <div className=" min-h-[calc(100vh)] bg-gradient-to-b from-AUIS-dark-teal to-AUIS-teal">
+      <Header />
+      <div className="flex">
+        <div className="hidden lg:flex flex-grow items-center justify-center xl:justify-end xl:pr-24">
+          <img src={auisLogo} className=" w-[350px] xl:w-[500px]"></img>
         </div>
-    );
+        <div className="flex flex-grow items-center justify-center xl:justify-start pb-12 xl:pl-24">
+          <SignUp afterSignUpUrl="/user-info" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SignUpScreen;
