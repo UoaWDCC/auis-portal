767dd5e13e4fef9935ae3d28ea3cc19754437c53
Tarun <36789694+Ratchet7x5@users.noreply.github.com>
2024-07-01 11:16:24 +0530

    update stripe publish key

--- web/src/screens/CheckoutScreen.tsx
+++ web/src/screens/CheckoutScreen.tsx
@@ -5,8 +5,7 @@ import {
   EmbeddedCheckout,
 } from "@stripe/react-stripe-js";
 
-const STRIPE_PUBLISHABLE_KEY = import.meta.env
-  .VITE_CLERK_PUBLISHABLE_KEYSTRIPE_PUBLISH_KEY_DEV;
+const STRIPE_PUBLISHABLE_KEY = import.meta.env.STRIPE_PUBLISH_KEY_DEV;
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
@@ -15,11 +14,14 @@ const stripePromise = loadStripe(`${STRIPE_PUBLISHABLE_KEY}`);
 function CheckoutScreen() {
   const fetchClientSecret = useCallback(async () => {
     // Create a Checkout Session
-    return await fetch("http://localhost:3000/api/stripe/create-checkout-session", {
-      method: "POST",
-      // add our own priceId here later for different products
-      body: JSON.stringify({ priceId: "price_1PSHWRP464csY2UpYpxvB2tk" }),
-    })
+    return await fetch(
+      "http://localhost:3000/api/stripe/create-checkout-session",
+      {
+        method: "POST",
+        // add our own priceId here later for different products
+        body: JSON.stringify({ priceId: "price_1PSHWRP464csY2UpYpxvB2tk" }),
+      }
+    )
       .then((res) => res.json())
       .then((data) => data.clientSecret);
   }, []);
