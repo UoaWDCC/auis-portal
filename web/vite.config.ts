3a5fe19716150875289badde5031c1a4eb530514
Tarun <36789694+Ratchet7x5@users.noreply.github.com>
2024-07-06 18:55:15 +0530

    implement new env_var loading and proxy config in vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,30 +1,34 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@utils": "/src/utils",
-      "@components": "/src/components",
-      "@pages": "/src/pages",
-      "@contexts": "/src/contexts",
-      "@layouts": "/src/layouts",
-      "@screens": "/src/screens",
-    },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000/", // process.env.PROXY_TARGET ??
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-        //rewrite: (path) => path.replace(/^\/api/, ""),
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@utils": "/src/utils",
+        "@components": "/src/components",
+        "@pages": "/src/pages",
+        "@contexts": "/src/contexts",
+        "@layouts": "/src/layouts",
+        "@screens": "/src/screens",
       },
     },
-  },
-});
+    server: {
+      proxy: {
+        "/api": {
+          target: process.env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+          //rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  });
+};
