9e3dea8edd75e74ad6159d21c6e7e177b6a26370
Devesh Duptala <96435676+duptala@users.noreply.github.com>
2024-07-11 19:01:58 +1200

    🔧 Fixed issue where it was showing duplicate events

--- web/src/components/SimpleSlider.tsx
+++ web/src/components/SimpleSlider.tsx
@@ -29,7 +29,7 @@ const SimpleSlider: React.FC<SimpleSliderProps> = ({ children, cardType }) => {
         settings: {
           slidesToShow: cardType === "past" ? 1 : 2,
           slidesToScroll: 1,
-          infinite: true,
+          infinite: false,
           dots: true,
         },
       },
@@ -61,27 +61,27 @@ const SimpleSlider: React.FC<SimpleSliderProps> = ({ children, cardType }) => {
         <Slider ref={sliderRef} {...settings}>
           {cardType === "past"
             ? React.Children.toArray(children)
-                .reduce((acc: any, child: any, index: number) => {
-                  if (index % 3 === 0) acc.push([]);
-                  acc[acc.length - 1].push(child);
-                  return acc;
-                }, [])
-                .map((group: any, index: number) => (
-                  <div key={index} className="p-2">
-                    <div className="flex flex-col">
-                      {group.map((child: any, subIndex: number) => (
-                        <div key={subIndex} className="p-2">
-                          {child}
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                ))
-            : React.Children.map(children, (child, index) => (
+              .reduce((acc: any, child: any, index: number) => {
+                if (index % 3 === 0) acc.push([]);
+                acc[acc.length - 1].push(child);
+                return acc;
+              }, [])
+              .map((group: any, index: number) => (
                 <div key={index} className="p-2">
-                  {child}
+                  <div className="flex flex-col">
+                    {group.map((child: any, subIndex: number) => (
+                      <div key={subIndex} className="p-2">
+                        {child}
+                      </div>
+                    ))}
+                  </div>
                 </div>
-              ))}
+              ))
+            : React.Children.map(children, (child, index) => (
+              <div key={index} className="p-2">
+                {child}
+              </div>
+            ))}
         </Slider>
       </div>
       <IoArrowForwardCircleOutline
