00c6628e9516b8702df408f3679c562ef0fba08b
Devesh Duptala <dduptala@gmail.com>
2024-07-11 12:39:58 +1200

    ✨Added upcoming events to the home screen for display

--- web/src/screens/HomeScreen.tsx
+++ web/src/screens/HomeScreen.tsx
@@ -2,13 +2,41 @@ import Hero from "@components/Hero";
 import Intro from "@components/Intro";
 import SomePhotos from "@components/SomePhotos";
 import UpcomingEvents from "@components/UpcomingEvents";
+import { GET_EVENTS, GET_EVENTS_GALLERY } from "../graphql/queries";
+import LoadingSpinner from "../components/LoadingSpinner";
+import type { Event } from "../types/types";
+import { useQuery } from "@apollo/client";
+import { Mapper } from "@utils/Mapper";
 
 export default function HomeScreen() {
+  const {
+    loading: eventsLoading,
+    data: eventsData,
+    error: eventsError,
+  } = useQuery(GET_EVENTS);
+
+  if (eventsLoading) {
+    return <LoadingSpinner />;
+  }
+
+  if (eventsError) {
+    return <div>CMS Offline</div>;
+  }
+
+  const events = Mapper.mapToEvents(eventsData);
+
+  const currentDate = new Date();
+
+  const upcomingEvents = events.filter((event) => {
+    const eventDate = new Date(event.eventDateStart);
+    return eventDate >= currentDate;
+  });
+
   return (
     <div>
       <Hero />
       <Intro />
-      <UpcomingEvents is_events_page={false} />
+      <UpcomingEvents upcomingEvents={upcomingEvents} />
       <SomePhotos />
     </div>
   );
