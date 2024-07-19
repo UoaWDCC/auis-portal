e5c40ae574e6b978a03c525a264417be75000bad
Ratchet7x5 <36789694+Ratchet7x5@users.noreply.github.com>
2024-07-19 19:26:16 +1200

    use new db schema in api/seed.ts

--- api/scripts/seed.ts
+++ api/scripts/seed.ts
@@ -1,31 +1,56 @@
-import { users } from "../schemas/User";
 import { db } from "../db/config/db";
+import { peoples, user_tickets, events } from "../schemas/schema";
 
 const main = async () => {
   try {
+    // Delete data
+    console.log("Deleting data in database");
+    await db.delete(peoples);
+    await db.delete(events);
+    await db.delete(user_tickets);
+
+    // Add users
     console.log("Seeding database");
-    // Delete all data
-    await db.delete(users);
-    await db.insert(users).values([
+
+    await db.insert(peoples).values([
       {
-        name: "Harsheel Singh",
-        description: "test",
-        email: "hsin212@aucklanduni.ac.nz",
-        year_of_study: 4,
+        id: 0,
+        email: "gury@go.at",
+        upi: "gmat",
+        university_id: "12345",
+        name: "Gury",
+        is_member: true,
+        member_expiry_date: new Date().toLocaleDateString(),
+        institution: "UoA",
+        year_of_study: "4",
       },
       {
-        name: "Guryash Matharu",
-        description: "test",
-        email: "gmat222@aucklanduni.ac.nz",
-        year_of_study: 4,
-      },
-      {
-        name: "Naren Rohan",
-        description: "test",
-        email: "nroh555@aucklanduni.ac.nz",
-        year_of_study: 4,
+        id: 2,
+        email: "naren@go.at",
+        upi: "nrnr",
+        university_id: "23456",
+        name: "Naren",
+        is_member: true,
+        member_expiry_date: new Date().toLocaleDateString(),
+        institution: "UoA",
+        year_of_study: "4",
       },
     ]);
+
+    // Add events
+    await db.insert(events).values([
+      {
+        id: 0,
+        title: "Gury's Cookout",
+        description: "Let him COOK",
+        event_date_start: new Date().toLocaleString(),
+        event_date_end: new Date().toLocaleString(),
+        event_capacity: 500,
+        is_live: false,
+        event_capacity_remaining: 500,
+      },
+    ]);
+
     process.exit(0);
   } catch (error) {
     console.error(error);
