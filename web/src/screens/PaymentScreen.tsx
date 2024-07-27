a36d4df81e558598a9c3831d7b64bd56a0b376eb
cbon848 <chalisabond@gmail.com>
2024-07-27 18:04:21 +1200

    updated payment screen
    How would I do the resizing so that it isn't blocky.

--- web/src/screens/PaymentScreen.tsx
+++ web/src/screens/PaymentScreen.tsx
@@ -4,48 +4,52 @@ import { MdEvent } from "react-icons/md";
 function PaymentScreen() {
   return (
     <>
-      <div className="mt-[8rem] min-h-screen px-10 sm:pl-[10rem]">
+    <div className="mt-[8rem] items-center pl-[4rem] pr-[5rem] md:pl-[10rem] md:pr-[10rem]">
         <h1
           data-testid="title"
-          className="pb-[1rem] pl-[1rem] text-5xl font-bold"
+          className="pb-[1rem] pl-[1rem] text-4xl md:text-5xl font-bold"
         >
           Review Order
         </h1>
         <div className="w-3/4 border-b border-gray-300"> </div>
         <div>
-          <h2 data-testid="subTitle" className="pt-[2rem] text-xl font-bold">
+          <h2 data-testid="subTitle" className="pt-[2rem] text-l md:text-xl font-bold">
             {" "}
             Two Semester Membership:
           </h2>
-          <li data-testid="firstListItem">
-            {" "}
-            Full-year <i>free</i> access to all AUIS events (except steins,
-            balls, and select events){" "}
-          </li>
-          <li data-testid="secondListItem">
-            {" "}
-            Discounted tickets to other host events
-          </li>
+          <h3 className="text-sm md:text-lg pt-[0.5rem]"> 
+            <li data-testid="firstListItem">
+              {" "}
+              Full-year <i>free</i> access to all AUIS events (except steins,
+              balls, and select events){" "}
+            </li>
+            <li data-testid="secondListItem">
+              {" "}
+              Discounted tickets to other host events
+            </li>
+          </h3>
         </div>
+      </div>
+      <div className="h-screen items-center pl-[1rem] pr-[5rem] md:pl-[10rem] md:pr-[10rem]">
         <div>
-          <div className="flex flex-row items-center justify-center space-x-3 pt-[1rem] sm:space-x-4 md:justify-end md:pr-[20rem]">
-            <div className="hidden lg:block">
+          <div className="flex flex-row items-center justify-center space-x-5 pt-[2rem] sm:justify-end lg:pr-[20rem]">
+            <div className="hidden md:block">
               <IoCartOutline data-testid="icon" size={70} color="black" />
             </div>
-            <h2 data-testid="price" className="text-xl font-[500]">
+            <h2 data-testid="price" className="txt-l md:text-xl font-[500] justify-center">
               {" "}
               $15.00 NZD{" "}
             </h2>
             <button
               data-testid="button"
-              className="h-10 rounded-xl bg-orange-400 px-4 text-xl font-bold text-white"
+              className="h-8 md:h-10 rounded-xl bg-orange-400 px-4 text-l md:text-xl font-bold text-white"
             >
               {" "}
               Continue{" "}
             </button>
           </div>
         </div>
-      </div>
+        </div>
     </>
   );
 }
