eefc309c8b4b82e797b3989892d5c1237ae57062
Guryash <gmat224@aucklanduni.ac.nz>
2024-08-05 19:07:33 +1200

    merged

--- web/src/screens/HomeScreen.tsx
+++ web/src/screens/HomeScreen.tsx
@@ -2,27 +2,41 @@ import Hero from "@components/Hero";
 import Intro from "@components/Intro";
 import SomePhotos from "@components/SomePhotos";
 import UpcomingEvents from "@components/UpcomingEvents";
-import { GET_EVENTS } from "../graphql/queries";
+import { GET_EVENTS, GET_EXECS } from "../graphql/queries";
 import LoadingSpinner from "../components/LoadingSpinner";
 import { useQuery } from "@apollo/client";
 import { Mapper } from "@utils/Mapper";
+import { useEffect, useState } from "react";
+import type { Event, Exec } from "../types/types";
 
 export default function HomeScreen() {
   const {
-    loading: eventsLoading,
-    data: eventsData,
-    error: eventsError,
+    loading: execsLoading,
+    data: execsData,
+    error: execsError,
   } = useQuery(GET_EVENTS);
 
-  if (eventsLoading) {
-    return <LoadingSpinner />;
-  }
 
-  if (eventsError) {
+  const [executives, setExecutives] = useState<Event[]>([]);
+  const [noExecs, setNoExecs] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (execsData) {
+      try {
+        const execs = Mapper.mapToExec(execsData);
+        setExecutives(execs);
+      } catch (error) {
+        setNoExecs(true);
+      }
+    }
+  }, [execsData]);
+
+  if (execsError || execsLoading) {
     return <div>CMS Offline</div>;
   }
 
-  const events = Mapper.mapToEvents(eventsData);
+  const events = Mapper.mapToEvents(execsData);
 
   const currentDate = new Date();
 
