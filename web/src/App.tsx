1ea5e0063583f0adcf2517ba3a7cd469c49dbb32
Guryash <gmat224@aucklanduni.ac.nz>
2024-09-11 13:43:52 +1200

    feat: add footer to all pages

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
-import Home from "./screens/HomeScreen";
 
 const App = () => {
   return (
     <>
       <main>
-        <div>
-          <Outlet />
-        </div>
+        <Outlet />
       </main>
+      <footer>
+        <Footer/>
+      </footer>
     </>
   );
 };
