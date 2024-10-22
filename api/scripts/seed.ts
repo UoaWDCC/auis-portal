569dbfdf1d1a2f60a962a544f0c56cd988c773df
Ratchet7x5 <36789694+Ratchet7x5@users.noreply.github.com>
2024-10-22 19:05:37 +1300

    update seed script to meet required schema

--- api/scripts/seed.ts
+++ api/scripts/seed.ts
@@ -20,9 +20,12 @@ const main = async () => {
         university_id: "12345",
         name: "Gury",
         is_member: true,
-        member_expiry_date: new Date().toLocaleDateString(),
+        member_expiry_date: new Date(
+          new Date().setFullYear(new Date().getFullYear() + 1)
+        ).toLocaleDateString(),
         institution: "UoA",
         year_of_study: "4",
+        status: "Domestic",
       },
       {
         id: 2,
@@ -31,9 +34,12 @@ const main = async () => {
         university_id: "23456",
         name: "Naren",
         is_member: true,
-        member_expiry_date: new Date().toLocaleDateString(),
+        member_expiry_date: new Date(
+          new Date().setFullYear(new Date().getMonth() + 6)
+        ).toLocaleDateString(),
         institution: "UoA",
         year_of_study: "4",
+        status: "International",
       },
     ]);
 
@@ -43,22 +49,26 @@ const main = async () => {
         id: 0,
         title: "Dance Series: Shawn Thomas",
         description: "Special dance series led by the goat himself, Guryash.",
+        location: "The dance floor baby. ",
         event_date_start: new Date().toLocaleString(),
         event_date_end: new Date().toLocaleString(),
         event_capacity: 20,
         is_live: true,
         event_capacity_remaining: 1,
+        terms_and_conditions: "No refunds. Gotta have moves like jagger.",
       },
       {
         id: 1,
         title: "Naren's Tech Workshop",
         description:
           "Informative workshop where Naren walksthrough the architecture of the AUIS App. Plenty of opportunity to network with sponsor companies and pizza is served. No pineapple pizzas.",
+        location: "Remotely from Naren's billion dollar bunker.",
         event_date_start: new Date().toLocaleString(),
         event_date_end: new Date().toLocaleString(),
         event_capacity: 5,
         is_live: true,
         event_capacity_remaining: 2,
+        terms_and_conditions: "Ain't no refunds out here cuz it's free.",
       },
       {
         id: 2,
@@ -67,7 +77,7 @@ const main = async () => {
         event_date_start: new Date().toLocaleString(),
         event_date_end: new Date().toLocaleString(),
         event_capacity: 20,
-        is_live: true,
+        is_live: false,
         event_capacity_remaining: 20,
       },
     ]);
