bfaccace133524014a2b1b43553a20c98241c666
Ratchet7x5 <36789694+Ratchet7x5@users.noreply.github.com>
2024-10-25 16:25:32 +1300

    refactor example file: getUsers.ts

--- api/gateway/getUsers.ts
+++ api/gateway/getUsers.ts
@@ -1,7 +1,7 @@
-import { users } from "../schemas/User";
+import { peoples } from "../schemas/schema";
 import { db } from "../db/config/db";
 import { User } from "../types/types";
 
-export async function getUsers() {
-  return (await db.select().from(users)) as User[];
+export async function getUsers(): Promise<User[]> {
+  return (await db.select().from(peoples)) as User[];
 }
