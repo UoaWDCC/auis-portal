90ee645b0bb8d3f00b7a0aef14bc4728ba8d1c3c
Ratchet7x5 <36789694+Ratchet7x5@users.noreply.github.com>
2024-10-25 16:03:26 +1300

    update user typing

--- api/types/types.ts
+++ api/types/types.ts
@@ -1,21 +1,18 @@
 export interface User {
+  id: number;
   name: string;
-  description: string;
   email: string;
-  year_of_study: number;
-}
-
-export interface UserComplex {
-  user_id: number;
-  email: string;
-  uoa_id?: string;
-  upi?: string;
-  institution?: string;
-  year?: string;
-  study_field?: string;
-  name: string;
-  is_admin: boolean;
-  is_paid: boolean;
-  is_info_confirmed: boolean;
-  created_at: Date;
+  university_id: string;
+  upi: string;
+  year_of_study: string;
+  study_field: string;
+  is_member: boolean;
+  status: string;
+  member_expiry_date: string;
+  institution: string;
+  created_at: string;
+  updated_at: string;
+  published_at: string;
+  created_by_id: number | null;
+  updated_by_id: number | null;
 }
