39db4696debe32c464cb35812f099e48507faee6
Ratchet7x5 <36789694+Ratchet7x5@users.noreply.github.com>
2024-10-27 22:47:47 +1300

    restore deleted drizzle schema config file and restore db:pull command

---
+++ api/drizzle-schema.config.ts
@@ -0,0 +1,17 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./schemas/*",
+  out: "./schemas",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+  introspect: {
+    casing: "preserve",
+  },
+  verbose: true,
+  strict: true,
+});
