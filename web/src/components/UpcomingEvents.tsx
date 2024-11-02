266d78829d352da72ee6ae6692040a2a1fcfeb08
Guryash <gmat224@aucklanduni.ac.nz>
2024-11-02 23:10:55 +1300

    feat: forward and back button for slider

--- web/src/components/UpcomingEvents.tsx
+++ web/src/components/UpcomingEvents.tsx
@@ -1,6 +1,12 @@
 import UpcomingEventsList from "./UpcomingEventsList";
 import { Event } from "../types/types";
-import { IoArrowBackCircleOutline, IoArrowForwardCircleOutline } from "react-icons/io5";
+import {
+  IoArrowBackCircleOutline,
+  IoArrowForwardCircleOutline,
+} from "react-icons/io5";
+import useScreenSize from "../hooks/useScreenSize";
+import { useState } from "react";
+import { useKeenSlider } from "keen-slider/react";
 
 interface UpcomingEventsProps {
   upcomingEvents: Event[];
@@ -8,33 +14,47 @@ interface UpcomingEventsProps {
 }
 
 const UpcomingEvents: React.FC<UpcomingEventsProps> = ({ upcomingEvents }) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const screenSize = useScreenSize();
+
+  const [sliderRef, instanceRef] = useKeenSlider({
+    initial: 0,
+    slideChanged(slider) {
+      setCurrentSlide(slider.track.details.rel);
+    },
+    mode: "free-snap",
+    slides: {
+      origin: "auto",
+      perView: screenSize.width > 640 ? (screenSize.width > 1000 ? 3 : 2) : 1,
+    },
+  });
+
   return (
     <div className="from-AUIS-dark-teal to-AUIS-teal bg-gradient-to-b pb-20">
       <h1 className="mx-3 py-10 text-center text-5xl font-bold text-white">
         Our Upcoming Events!
       </h1>
       <div className="mx-auto flex items-center justify-center">
-      <LeftArrow
-            onClick={(e: any) =>
-              console.log("clicked")
-              // e.stopPropagation() || instanceRef.current?.next()
-            }
-            disabled={false
-              // currentSlide >=
-              // instanceRef.current.track.details.slides.length - (screenSize.width > 640 ? (screenSize.width > 1000 ? 3 : 2) : 1)
-            }
-          />
-        <UpcomingEventsList upcomingEvents={upcomingEvents} />
+        <LeftArrow
+          onClick={(e: any) =>
+            e.stopPropagation() || instanceRef.current?.prev()
+          }
+          disabled={currentSlide === 0}
+        />
+        <UpcomingEventsList
+          sliderRef={sliderRef}
+          upcomingEvents={upcomingEvents}
+        />
         <RightArrow
-            onClick={(e: any) =>
-              console.log("clicked")
-              // e.stopPropagation() || instanceRef.current?.next()
-            }
-            disabled={false
-              // currentSlide >=
-              // instanceRef.current.track.details.slides.length - (screenSize.width > 640 ? (screenSize.width > 1000 ? 3 : 2) : 1)
-            }
-          />
+          onClick={(e: any) =>
+            e.stopPropagation() || instanceRef.current?.next()
+          }
+          disabled={
+            currentSlide >=
+            upcomingEvents.length -
+              (screenSize.width > 640 ? (screenSize.width > 1000 ? 3 : 2) : 1)
+          }
+        />
       </div>
     </div>
   );
@@ -45,7 +65,7 @@ function LeftArrow(props: { disabled: boolean; onClick: (e: any) => void }) {
   return (
     <IoArrowBackCircleOutline
       onClick={props.onClick}
-      className={`arrow mx-4 hidden h-16 w-16 text-gray-400 hover:cursor-pointer sm:flex ${disabled ? "opacity-65" : " "}`}
+      className={`arrow mx-4 hidden h-16 w-16 text-gray-400 hover:cursor-pointer sm:flex ${disabled ? "opacity-40" : "hover:scale-110"}`}
     />
   );
 }
@@ -55,7 +75,7 @@ function RightArrow(props: { disabled: boolean; onClick: (e: any) => void }) {
   return (
     <IoArrowForwardCircleOutline
       onClick={props.onClick}
-      className={`arrow mx-4 hidden h-16 w-16 text-gray-400 hover:cursor-pointer sm:flex ${disabled ? "opacity-65" : " "}`}
+      className={`arrow mx-4 hidden h-16 w-16 text-gray-400 hover:cursor-pointer sm:flex ${disabled ? "opacity-40" : "hover:scale-110"}`}
     />
   );
 }
--- web/src/components/UpcomingEventsList.tsx
+++ web/src/components/UpcomingEventsList.tsx
@@ -2,54 +2,19 @@ import { Event } from "../types/types";
 import UpcomingEventCard from "./UpcomingEventCard";
 import { Link } from "react-router-dom";
 import "keen-slider/keen-slider.min.css";
-import { useKeenSlider } from "keen-slider/react";
-import { useState } from "react";
-import {
-  IoArrowBackCircleOutline,
-  IoArrowForwardCircleOutline,
-} from "react-icons/io5";
-import useScreenSize from "../hooks/useScreenSize";
+
 
 interface UpcomingEventsListProps {
-  upcomingEvents: Event[];
+  upcomingEvents: Event[],
+  sliderRef: (node: HTMLElement | null) => void;
 }
 
 const UpcomingEventsList: React.FC<UpcomingEventsListProps> = ({
   upcomingEvents,
+  sliderRef
 }) => {
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const [loaded, setLoaded] = useState(false);
-  const screenSize = useScreenSize();
-
-  const [sliderRef, instanceRef] = useKeenSlider({
-    // loop: true,
-    initial: 0,
-    slideChanged(slider) {
-      setCurrentSlide(slider.track.details.rel);
-    },
-    mode: "free-snap",
-    created() {
-      setLoaded(true);
-    },
-    slides: {
-      origin: "auto",
-      perView: screenSize.width > 640 ? (screenSize.width > 1000 ? 3 : 2) : 1,
-      // spacing: 15,
-    },
-  });
-
   return (
     <>
-      {/* {loaded && instanceRef.current && (
-        <>
-          <LeftArrow
-            onClick={(e: any) =>
-              e.stopPropagation() || instanceRef.current?.prev()
-            }
-            disabled={currentSlide === 0}
-          />
-        </>
-      )} */}
       <div ref={sliderRef} className="keen-slider">
         {upcomingEvents.map((event) => (
           <div
@@ -62,43 +27,8 @@ const UpcomingEventsList: React.FC<UpcomingEventsListProps> = ({
           </div>
         ))}
       </div>
-      
-      {/* Loaded is needed to make sure all slider slides are loaded in */}
-      {/* {loaded && instanceRef.current && (
-        <>
-          <RightArrow
-            onClick={(e: any) =>
-              e.stopPropagation() || instanceRef.current?.next()
-            }
-            disabled={
-              currentSlide >=
-              instanceRef.current.track.details.slides.length - (screenSize.width > 640 ? (screenSize.width > 1000 ? 3 : 2) : 1)
-            }
-          />
-        </>
-      )} */}
     </>
   );
 };
-
-function LeftArrow(props: { disabled: boolean; onClick: (e: any) => void }) {
-  const disabled = props.disabled ? " arrow--disabled" : "";
-  return (
-    <IoArrowBackCircleOutline
-      onClick={props.onClick}
-      className={`arrow mx-4 hidden h-16 w-16 text-gray-400 hover:cursor-pointer sm:flex ${disabled ? "opacity-65" : " "}`}
-    />
-  );
-}
-
-function RightArrow(props: { disabled: boolean; onClick: (e: any) => void }) {
-  const disabled = props.disabled ? " arrow--disabled" : "";
-  return (
-    <IoArrowForwardCircleOutline
-      onClick={props.onClick}
-      className={`arrow mx-4 hidden h-16 w-16 text-gray-400 hover:cursor-pointer sm:flex ${disabled ? "opacity-65" : " "}`}
-    />
-  );
-}
 
 export default UpcomingEventsList;
