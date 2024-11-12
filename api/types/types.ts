564cee9dea6f810217bf6c775a9fe246235d0766
Ratchet7x5 <36789694+Ratchet7x5@users.noreply.github.com>
2024-11-12 23:50:03 +1300

    chore: update types

--- api/types/types.ts
+++ api/types/types.ts
@@ -2,17 +2,17 @@ export interface User {
   id: number;
   name: string;
   email: string;
-  university_id: string;
+  universityId: string;
   upi: string;
-  year_of_study: string;
-  study_field: string;
-  is_member: boolean;
+  yearOfStudy: string;
+  studyField: string;
+  isMember: boolean;
   status: string;
-  member_expiry_date: string;
+  memberExpiryDate: string;
   institution: string;
-  created_at: string;
-  updated_at: string;
-  published_at: string;
-  created_by_id: number | null;
-  updated_by_id: number | null;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  createdById: number | null;
+  updatedById: number | null;
 }
