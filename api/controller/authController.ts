aafb003866e518098aaeebd18c295b206d2aa2c4
Ratchet7x5 <36789694+Ratchet7x5@users.noreply.github.com>
2024-11-16 17:50:41 +1300

    chore: cleanup old clerk code from AuthRoutes and AuthController

--- api/controller/authController.ts
+++ api/controller/authController.ts
@@ -2,29 +2,6 @@ import { Request, Response } from "express";
 import asyncHandler from "../middleware/asyncHandler";
 import { insertUserByEmail, deleteUserByEmail } from "../gateway/getUsers";
 
-export const signUp = asyncHandler(async (req: Request, res: Response) => {
+export const signOut = asyncHandler(async (req: Request, res: Response) => {
   throw new Error("Not implemented yet");
 });
-
-export const logIn = asyncHandler(async (req: Request, res: Response) => {
-  throw new Error("Not implemented yet");
-});
-
-export const handleWebhook = asyncHandler(
-  async (req: Request, res: Response) => {
-    try {
-      if (req.body.type == "user.created") {
-        const newUserEmail: string =
-          req.body.data.email_addresses[0].email_address;
-        const newUser = await insertUserByEmail(newUserEmail);
-        res.json({ received: true });
-      } else if (req.body.type == "user.deleted") {
-        res.json({ received: true });
-      } else if (req.body.type == "user.updated") {
-        res.json({ received: true });
-      }
-    } catch (error) {
-      res.status(500).send(`Webhook error: ${error}`);
-    }
-  }
-);
--- api/routes/authRoutes.ts
+++ api/routes/authRoutes.ts
@@ -1,10 +1,8 @@
 import express from "express";
-import { signUp, logIn, handleWebhook } from "../controller/authController";
+import { signOut } from "../controller/authController";
 
 const router = express.Router();
 
-router.post("/sign-up", signUp);
-router.post("/sign-in", logIn);
-router.post("/webhook", handleWebhook);
+router.post("/signout", signOut);
 
 export default router;
