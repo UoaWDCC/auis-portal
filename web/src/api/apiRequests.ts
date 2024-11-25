f704577d4222d471779d2a5d057825eac0411202
Ratchet7x5 <36789694+Ratchet7x5@users.noreply.github.com>
2024-11-25 23:03:33 +1300

    chore: add new fetch functions to apiRequests.ts in frontend

--- web/src/api/apiRequests.ts
+++ web/src/api/apiRequests.ts
@@ -11,3 +11,45 @@ export const fetchUser = async (): Promise<User[]> => {
   // console.log(response.data[0].memberExpiryDate)
   return response.data;
 };
+
+//this specific function is experimental. Slightly unsafe to use. Conditionally creates a event checkout, or membership checkout session.
+export const fetchCheckoutSecret = async (payload: {
+  stripeKey: string;
+  isEventTicket: string;
+}): Promise<string> => {
+  return await fetch("/api/stripe/create-event-checkout", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    // add our own priceId here later for different products
+    body: JSON.stringify(payload),
+  })
+    .then((res) => res.json())
+    .then((data) => data.clientSecret);
+};
+
+// the ones below are for a separate approach. Safe to use.
+export const fetchEventCheckoutSecret = async (payload: {
+  stripeKey: string;
+}): Promise<string> => {
+  return await fetch("/api/stripe/create-event-checkout", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    // add our own priceId here later for different products
+    body: JSON.stringify(payload),
+  })
+    .then((res) => res.json())
+    .then((data) => data.clientSecret);
+};
+
+export const fetchMembershipCheckoutSecret = async (payload: {
+  stripeKey: string;
+}): Promise<string> => {
+  return await fetch("/api/stripe/create-membership-checkout", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    // add our own priceId here later for different products
+    body: JSON.stringify(payload),
+  })
+    .then((res) => res.json())
+    .then((data) => data.clientSecret);
+};
